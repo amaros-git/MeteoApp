5e1048d25a217f9b6c7774302a953bfa43800428
AnatolijsM <a.morozovs@gmail.com>
2021-02-28 14:57:08 +0200

    fix of key storage

--- app/src/main/java/lv/maros/meteoapp/data/network/CitiesHttpClient.kt
+++ app/src/main/java/lv/maros/meteoapp/data/network/CitiesHttpClient.kt
@@ -1,6 +1,8 @@
 package lv.maros.meteoapp.data.network
 
+import lv.maros.meteoapp.BuildConfig
 import okhttp3.OkHttpClient
+import java.util.*
 
 /**
  * For https://rapidapi.com/wirefreethought/api/geodb-cities/endpoints
@@ -9,7 +11,7 @@ class CitiesHttpClient : OkHttpClient() {
 
     companion object {
 
-        private const val HEADER_API_KEY = "55964d3013msh8838d26afee1273p1f33a5jsna40d4e410dc2"
+        private const val HEADER_API_KEY = BuildConfig.RAPID_KEY
         private const val HEADER_RAPID_HOST = "wft-geo-db.p.rapidapi.com"
         private const val HEADER_USE_QUERY_STRING = true
 
